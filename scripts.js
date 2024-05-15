673ebff09fad04abc4afcc2586012002044df979
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-15 14:51:08 -0400

    Add basic logic for numbers and operators
    This includes event listeners for each "button"
    It excludes the equals button and other functional buttons

--- scripts.js
+++ scripts.js
@@ -11,5 +11,128 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
+    if (y === 0) {
+
+    }
     return x / y;
 }
+
+function convertNumber() {
+    currentNumber = parseInt(newNumber);
+    newNumber = "";
+    ongoing = true;
+}
+
+function performOperation() {
+    switch (operator) {
+        case "+":
+            currentNumber = add(currentNumber, newNumber);
+            newNumber = "";
+            ongoing = true;
+            break;
+        case "-":
+            currentNumber = subtract(currentNumber, newNumber);
+            newNumber = "";
+            ongoing = true;
+            break;
+        case "*":
+            currentNumber = multiply(currentNumber, newNumber);
+            newNumber = "";
+            ongoing = true;
+            break;
+        case "/":
+            currentNumber = divide(currentNumber, newNumber);
+            newNumber = "";
+            ongoing = true;
+            break;
+        default:
+            console.error("Failed to read operator");
+    }
+}
+
+function clear() {
+    currentNumber = 0;
+    newNumber = 0;
+    ongoing = false;
+    operator = "+";
+    displayString = "";
+}
+
+let currentNumber = 0;
+let newNumber = "";
+let ongoing = false;
+let operator = "+";
+let displayString = "";
+
+const display = document.querySelector(".display");
+display.textContent = displayString;
+
+const numberButtons = document.querySelectorAll(".numberButtons");
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        displayString += button.id;
+        newNumber += button.id;
+        display.textContent = displayString;
+    });
+});
+
+const operatorButtons = document.querySelectorAll(".operatorButtons");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (ongoing) {
+            switch (button.id) {
+                case "add":
+                    performOperation();
+                    operator = "+";
+                    displayString = currentNumber + operator;
+                    break;
+                case "subtract":
+                    performOperation();
+                    operator = "-";
+                    displayString = currentNumber + operator;
+                    break;
+                case "multiply":
+                    performOperation();
+                    operator = "*";
+                    displayString = currentNumber + operator;
+                    break;
+                case "divide":
+                    performOperation();
+                    operator = "/";
+                    displayString = currentNumber + operator;
+                    break;
+                case "equals":
+                    break;
+                default:
+                    console.error("Failed to read id of operator button");
+            }
+            display.textContent = displayString;
+        } else {
+            switch (button.id) {
+                case "add":
+                    convertNumber();
+                    operator = "+";
+                    displayString += operator;
+                    break;
+                case "subtract":
+                    convertNumber();
+                    operator = "-";
+                    displayString += operator;
+                    break;
+                case "multiply":
+                    convertNumber();
+                    operator = "*";
+                    displayString += operator;
+                    break;
+                case "divide":
+                    convertNumber();
+                    operator = "/";
+                    displayString += operator;
+                    break;
+                default:
+                    console.error("Failed to read id of operator button");
+            }
+            display.textContent = displayString;
+        }
+    });
+});
